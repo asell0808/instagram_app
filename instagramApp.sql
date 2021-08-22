fa5030f45397dbfed731a7ca91c3707d2e60e744
Anthony Sellers <asell0808@yahoo.com>
2021-08-22 13:34:29 -0700

    Update instagramApp.sql

--- instagramApp.sql
+++ instagramApp.sql
@@ -1,3 +1,80 @@
 -- Section 14, building an instagram DB clone 
+-- This file will have our tables for the database to function with the data values.
+-- This file will also have our data and our exercises to demonstrate joining all of the tables together.
 
 CREATE DATABASE instagram_app;
+USE instagram_app;
+
+-- user schema table 
+CREATE TABLE users
+(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+-- photo schema table 
+CREATE TABLE photos
+(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+-- Comments schema table 
+CREATE TABLE comments
+(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+-- Like schema table 
+CREATE TABLE likes
+(    
+    -- likes table does not need an ID becuase it is associated to the user who like a picture, and therefore, its primary key can be the users primary key, and the photo key combined 
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
+
+-- Followers schema table 
+CREATE TABLE follows
+(
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
+-- Hashtag schema table 
+-- solutions for tags will require you use on of three ways of implementing tags
+-- we will be using 2 tables, but you can also implement them with 1 table or 3 tables 
+CREATE TABLE tags 
+(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags 
+(
+    photo_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
+
+
