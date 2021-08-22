697d1a73d062b267c5f2920f9897aa288eb6482a
Anthony Sellers <asell0808@yahoo.com>
2021-08-22 14:47:47 -0700

    Update instagramApp.sql

--- instagramApp.sql
+++ instagramApp.sql
@@ -78,6 +78,99 @@ CREATE TABLE photo_tags
 -- Check all of our tables 
 SHOW tables;
 
+-- Exercises 
+
+-- 1. Finding the 5 oldest users 
+SELECT * FROM users
+ORDER BY created_at
+LIMIT 5;
+
+-- 2. What day of the week do most users register on 
+-- we need it to schedule a low budget add campaign 
+SELECT 
+    DAYNAME(created_at) AS day,
+    COUNT(*) AS total
+FROM users
+GROUP BY day
+ORDER BY total DESC;
+
+-- add a limit
+SELECT 
+    DAYNAME(created_at) AS day,
+    COUNT(*) AS total
+FROM users
+GROUP BY day
+ORDER BY total DESC
+LIMIT 2;
+
+-- 3.  We want to target our inactive users with an email account 
+-- Find the users who have never posted a photo 
+SELECT * 
+FROM users;
+
+SELECT * 
+FROM photos;
+
+SELECT 
+username,
+image_url
+FROM users
+LEFT JOIN photos 
+    ON users.id = photos.user_id
+WHERE photos.id IS NULL;
+    
+-- 4. we are running a new contest to see who can get the most likes ona  single photo. Who won?
+SELECT * 
+From photos;
+SELECT * 
+From likes;
+
+SELECT 
+    username,
+    photos.id, 
+    photos.image_url, 
+    COUNT(*) AS total 
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
+
+-- 5. How many times does the average user post 
+SELECT
+(SELECT 
+    COUNT(*) 
+FROM photos)
+/
+(SELECT 
+    COUNT(*) 
+FROM users)
+AS 'AVG';
+
+-- 6. What are the top 5 most commonly used hashtags 
+SELECT
+    tags.tag_name,
+    COUNT(*) AS 'total'
+FROM photo_tags
+JOIN tags
+    ON photo_tags.tag_id = tags.id
+GROUP BY tags.id
+ORDER BY total DESC
+LIMIT 5;
+
+-- 7. Find the bot accounts in our database 
+SELECT 
+    username,
+    COUNT(*) AS num_likes
+FROM users
+INNER JOIN likes
+    ON users.id = likes.user_id
+GROUP BY likes.user_id
+HAVING num_likes = (SELECT COUNT(*) FROM photos); -- HAVING is a new command that works like where, but it allows you to filter your data in GROUP BY
+
 
 -- Count our data example because our cli will not display all of the data at once 
 SELECT COUNT(*) FROM likes;
